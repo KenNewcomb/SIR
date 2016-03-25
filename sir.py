fa4c7ec66ac5a35d9ddbe9f40af1400f635631ac
Ken Newcomb <KenNewcombNJ@gmail.com>
2016-03-25 10:15:51 -0400

    Bug: Did not multiply by dt.

--- sir.py
+++ sir.py
@@ -3,12 +3,13 @@ import sys
 
 def usage():
 	print('usage: python3 sir.py beta gamma s0 i0 r0 dt timesteps')
+	print('Note: s(t) + i(t) + r(t) = 1.')
 
 def integrate(beta, gamma, s0, i0, r0, dt):
 	
 	snew = -beta*s0*i0*dt + s0
-	inew = beta*s0*i0-gamma*i0 + i0
-	rnew = gamma*i0 + r0
+	inew = (beta*s0*i0-gamma*i0)*dt + i0
+	rnew = 1-snew-inew
 
 	return [snew, inew, rnew] 
 
@@ -27,7 +28,7 @@ gamma = float(sys.argv[2])
 # Initial populations
 s0    = float(sys.argv[3])
 i0    = float(sys.argv[4])
-r0    = float(sys.argv[5]) 
+r0    = float(sys.argv[5])
 
 # Options
 dt        = float(sys.argv[6])
@@ -36,6 +37,12 @@ timesteps = int(sys.argv[7])
 s = []
 i = []
 r = []
+
+# Verify that S+I+R=1
+if s0 + i0 + r0 != 1.0:
+	usage()
+	exit()
+ 
 for step in range(0, timesteps):
 	[snew, inew, rnew]  = integrate(beta, gamma, s0, i0, r0, dt)
 	s.append(snew)
